ec73d7f78a071be5c8ff64e6204658ef9a24a6ac
nostroguardian <shmelivan5@yandex.ru>
2025-02-21 21:26:31 +0300

    chakraui integrated

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider as ChakraProvider } from '@/components/ui/provider';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<ChakraProvider>
+			<App />
+		</ChakraProvider>
 	</StrictMode>
 );
