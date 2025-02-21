0472a223f69668e35434b35b3503457c30b12b47
nostroguardian <shmelivan5@yandex.ru>
2025-02-21 21:33:34 +0300

    perfectionist eslint plugin

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import { Provider as ChakraProvider } from '@/components/ui/provider';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider as ChakraProvider } from '@/components/ui/provider';
+
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
